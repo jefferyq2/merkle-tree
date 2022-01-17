73f6df1fcb09646b5102423d3f2755fc10e7c72f
John Davies <jd.other@hotmail.com>
2022-01-17 11:08:54 +0000

    initial prompt and input validation

--- merkle-tree.py
+++ merkle-tree.py
@@ -2,7 +2,8 @@ import hashlib
 import math
 
 """
-- Prompt the user, asking for the number of messages to add to the tree (must be even and below a maximum)
+- Prompt the user, asking for the number of messages to add to the tree (must be even, a power of 2 and below a maximum)
+- Reject invalid input
 - Assign the result to the variable message_count
 - Initialise the variable layer_count to ((log message_count base 2) + 2)
 - Initialise a 'tree' list. It should have layer_count number of elements. Each element is a list, so 'tree' is actually a 2d array
@@ -23,3 +24,18 @@ import math
 
 Future: Set up an interactive challenge and response between Alice and Bob - Bob asks for proof of a given message, Alice provide the necessary hashes
 """
+
+# - Prompt the user, asking for the number of messages to add to the tree (must be even, a power of 2 and below a maximum)
+# - Edit: only allow 8, 16 or 32 as valid responses
+# - Reject invalid input
+# - Assign the result to the variable message_count
+
+message_count = None
+
+while (message_count != 8) and (message_count != 16) and (message_count != 32):
+    try:
+        message_count = int(input('How many messages (leaf nodes) will be added to the tree? (Type \'8\',\'16\' or \'32\')\n'))
+    except:
+        print('Invalid input')
+
+print(f'Message count: {message_count}')
